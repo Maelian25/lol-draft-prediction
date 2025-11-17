80181ca0245bda6286cfff1af77ff0f570ef7f88
maelian25 <vuillemenotmaelian@gmail.com>
2025-11-17 18:41:12 +0100

    Added a similar fn to check on target roles inegrity

--- src/utils/data_integrity.py
+++ src/utils/data_integrity.py
@@ -3,7 +3,7 @@ import numpy as np
 from src.utils.general_helper import save_file
 
 
-def check_target_intergity(matches_states):
+def check_target_pick_intergity(matches_states):
     """
     Provide a check on dataset to see if every target ban and pick are available
     (which they should be)
@@ -28,6 +28,38 @@ def check_target_intergity(matches_states):
     problem_rows = matches_states[
         ~matches_states["pick_available"] | ~matches_states["ban_available"]
     ]
+
+    print(f"Unavailable targets: {len(problem_rows)}")
+
+    # Save file
+    save_file(problem_rows, location=None, filename="targets_not_in_mask.csv")
+
+
+def check_target_role_intergity(matches_states):
+    """
+    Provide a check on dataset to see if every target ban and pick are available
+    (which they should be)
+    """
+
+    def is_target_available(row, target_col):
+        target = row[target_col]
+        side = row["next_side"]
+        if np.isnan(target):
+            return True
+        target = int(target)
+
+        if side == 1:
+            return row["blue_roles_available"][target] == 1
+        else:
+            return row["red_roles_available"][target] == 1
+
+    # Columns check
+    matches_states["role_available"] = matches_states.apply(
+        lambda row: is_target_available(row, "target_role"), axis=1
+    )
+
+    # Filter on the matches states file
+    problem_rows = matches_states[~matches_states["role_available"]]
 
     print(f"Unavailable targets: {len(problem_rows)}")
 
