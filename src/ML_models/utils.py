8cacc8795b0ff7d11fee0ffe2db907be4253bb6b
maelian25 <vuillemenotmaelian@gmail.com>
2025-11-22 03:05:10 +0100

    Changed model blocks to use residual blocks instead

--- src/ML_models/utils.py
+++ src/ML_models/utils.py
@@ -1,48 +1,34 @@
 import torch.nn as nn
 
 
-# MLP Model blocks
-def two_layers_mlp(hidden_dim, dropout, output_size):
+def head_mlp(type: str, hidden_dim, output_size):
 
-    mlp_block = nn.Sequential(
-        nn.Linear(hidden_dim // 2, hidden_dim // 4),
-        nn.LayerNorm(hidden_dim // 4),
-        nn.GELU(),
-        nn.Dropout(dropout * 0.5),
-        nn.Linear(hidden_dim // 4, output_size),
-    )
-    return mlp_block
+    match (type):
+        case "pick" | "ban":
+            mlp = nn.Sequential(
+                nn.Linear(hidden_dim, hidden_dim // 2),
+                nn.GELU(),
+                nn.LayerNorm(hidden_dim // 2),
+                nn.Linear(hidden_dim // 2, output_size),
+            )
+        case "role":
+            mlp = nn.Sequential(
+                nn.Linear(hidden_dim, hidden_dim // 4),
+                nn.GELU(),
+                nn.Linear(hidden_dim // 4, output_size),
+            )
+        case "wr":
+            mlp = nn.Sequential(
+                nn.Linear(hidden_dim, hidden_dim // 4),
+                nn.GELU(),
+                nn.Linear(hidden_dim // 4, output_size),
+            )
+        case _:
+            mlp = nn.Sequential(
+                nn.Linear(hidden_dim, hidden_dim // 2),
+                nn.GELU(),
+                nn.LayerNorm(hidden_dim // 2),
+                nn.Linear(hidden_dim // 2, output_size),
+            )
 
-
-def three_layers_mlp(hidden_dim, dropout, output_size):
-
-    mlp_block = nn.Sequential(
-        nn.Linear(hidden_dim // 2, hidden_dim // 2),
-        nn.LayerNorm(hidden_dim // 2),
-        nn.GELU(),
-        nn.Dropout(dropout),
-        nn.Linear(hidden_dim // 2, hidden_dim // 4),
-        nn.LayerNorm(hidden_dim // 4),
-        nn.GELU(),
-        nn.Dropout(dropout),
-        nn.Linear(hidden_dim // 4, output_size),
-    )
-    return mlp_block
-
-
-def shared_mlp(input_dim, hidden_dim, dropout):
-
-    mlp_block = nn.Sequential(
-        nn.Linear(input_dim, hidden_dim),
-        nn.LayerNorm(hidden_dim),
-        nn.GELU(),
-        nn.Dropout(dropout),
-        nn.Linear(hidden_dim, hidden_dim),
-        nn.LayerNorm(hidden_dim),
-        nn.GELU(),
-        nn.Dropout(dropout),
-        nn.Linear(hidden_dim, hidden_dim // 2),
-        nn.LayerNorm(hidden_dim // 2),
-        nn.GELU(),
-    )
-    return mlp_block
+    return mlp
